dbec456ec31f98d49ce07b314320a99c7a9bd07d
satya-08 <23a81a4383@sves.org.in>
2026-08-24 19:48:52 +0530

    Static Block Concepts

---
+++ src/StaticExample/StaticBlock.java
@@ -0,0 +1,25 @@
+package StaticExample;
+
+
+// Initialization of static attributes and methods
+public class StaticBlock {
+	static int a=4;
+	static int b;
+	
+	// Runs at the main class loaded at 1st object is created only once
+	static {
+		System.out.println("Hii your class Loaded");
+		b=a*5;
+	}
+	
+	public static void main(String[] args) {
+		StaticBlock bl1=new StaticBlock();
+		System.out.println(StaticBlock.a+" "+StaticBlock.b);
+		StaticBlock.b+=5;
+		StaticBlock bl2=new StaticBlock();
+		System.out.println(StaticBlock.a+" "+StaticBlock.b);
+		
+		
+	}
+
+}
